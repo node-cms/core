77bd05dfb0d4f38250e83955a9e7732f427bd1cd
Roman Eckert <roman.eckert@inblau.de>
2020-02-12 20:45:22 +0100

    update jest conf

--- packages/core/service/application.service.ts
+++ packages/core/service/application.service.ts
@@ -2,8 +2,6 @@ import FileSystemUtil from '@node-cms/file-system/util/file-system.util';
 
 export default class ApplicationService {
     public static async start(): Promise<void> {
-        console.log('start dev');
-
         await FileSystemUtil.isDirectory(__dirname);
     }
 }
